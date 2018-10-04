61f330bf7296977f770d29f1e44ce7ba42c32257
Ihor Chyzh <ichyzh@gmail.com>
2018-10-04 21:35:00 +0300

    Lesson 3, math.

---
+++ lesson03-math/script.js
@@ -0,0 +1,14 @@
+var sum = 4 + 5;  //9
+var difference = 4 - 5; // -1
+var product = 4 * 5; // 20
+var quotient = 4 / 5; // 0.8
+
+var foo = 4 + 5 * 6 - 8; // 26
+
+var bar = foo + 1; // 27
+
+
+var foo2 = parseInt("314", 10); // 314
+console.log(foo2);
+var foo3 = parseFloat("3.14", 10); // 3.14
+console.log(foo3);
