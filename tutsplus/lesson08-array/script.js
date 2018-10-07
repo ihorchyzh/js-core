2cb3b93182df196bc55d2a5bd0161c079cac7739
Ihor Chyzh <ichyzh@gmail.com>
2018-10-07 14:54:50 +0300

    Formatted code.

--- tutsplus/lesson08-array/script.js
+++ tutsplus/lesson08-array/script.js
@@ -9,13 +9,11 @@ foo[1] = 13;
 var foo = [11, "hello", true];
 foo[3] = 15;
 foo[4] = "5th value";
-foo[foo.length]="a new value";
-foo[foo.length]="one more new value";
+foo[foo.length] = "a new value";
+foo[foo.length] = "one more new value";
 foo.push("one more more new value");
 
 alert(foo);
-
-
 
 
 var names = ["Jeremy", "Jeffrey"],
